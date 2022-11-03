4161d529467b649fe7d14d273f96ef55952cc3ae
IanBaynard <ianbay@udel.edu>
2022-11-03 00:13:10 -0400

    Week9 Homework

---
+++ Week9-OOP/Assets/Scripts/GreenBallCollisionManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+    public GameObject yellowBallPrefab;
+    public GameObject plane; 
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Green Ball Collision Manager Function");
+        if (OtherBall.GetComponent<BlueBallCollisionManager>() != null)
+        {
+            Debug.Log("Green Collided With Blue");
+
+            Instantiate(yellowBallPrefab, plane.transform);
+        }
+    }
+
+    
+}
+
+
---
+++ Week9-OOP/Assets/Scripts/RedBallCollisionManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class RedBallCollisionManager : BallCollisionManager
+{
+    public GameObject blueBallPrefab;
+    public GameObject yellowBallPrefab;
+    public GameObject plane;
+
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Red Ball Collision Manager Function");
+        if (OtherBall.GetComponent<GreenBallCollisionManager>() != null)
+        {
+            Debug.Log("Red Collided With Green");
+            //Since Red Collided with Green it will Destory Green 
+            Destroy(OtherBall);
+
+        }
+        if (OtherBall.GetComponent<YellowBallCollisionManager>() != null)
+        {
+            Debug.Log("Red Collided With Yellow");
+            //Since Red Collided with Yellow it will change the color to black
+            OtherBall.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
+        }
+
+    }
+}
+
