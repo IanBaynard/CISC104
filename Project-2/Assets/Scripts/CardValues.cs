dc671fcd7ca8483d9bffcc484a21f075a76b860a
IanBaynard <ianbay@udel.edu>
2022-12-05 23:02:44 -0500

    Project-2
    Ian,Shaina,Larissa, Abigail, Fi

---
+++ Project-2/Assets/Scripts/CardValues.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace card_game_go_fish
+{
+    enum Values
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13,
+    }
+}
